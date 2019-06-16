0ec8b859f2b268b70aabfcfcef12ee82be504496
Benjamin Mann <8enmann@gmail.com>
2019-06-16 13:36:19 -0700

    update according to v3 of paper, fix #3

--- pytorch_lamb/lamb.py
+++ pytorch_lamb/lamb.py
@@ -14,10 +14,9 @@ def log_lamb_rs(optimizer: Optimizer, event_writer: SummaryWriter, token_count: 
     for group in optimizer.param_groups:
         for p in group['params']:
             state = optimizer.state[p]
-            for i in ('r', 'r1', 'r2'):
-                if i not in state:
-                    continue
-                results[i].append(state[i])
+            for i in ('weight_norm', 'adam_norm', 'trust_ratio'):
+                if i in state:
+                    results[i].append(state[i])
 
     for k, v in results.items():
         event_writer.add_histogram(f'lamb/{k}', torch.tensor(v), token_count)
@@ -25,7 +24,7 @@ def log_lamb_rs(optimizer: Optimizer, event_writer: SummaryWriter, token_count: 
 class Lamb(Optimizer):
     r"""Implements Lamb algorithm.
 
-    It has been proposed in `Reducing BERT Pre-Training Time from 3 Days to 76 Minutes`_.
+    It has been proposed in `Large Batch Optimization for Deep Learning: Training BERT in 76 minutes`_.
 
     Arguments:
         params (iterable): iterable of parameters to optimize or dicts defining
@@ -39,11 +38,11 @@ class Lamb(Optimizer):
         adam (bool, optional): always use trust ratio = 1, which turns this into
             Adam. Useful for comparison purposes.
 
-    .. _Reducing BERT Pre-Training Time from 3 Days to 76 Minutes:
+    .. _Large Batch Optimization for Deep Learning: Training BERT in 76 minutes:
         https://arxiv.org/abs/1904.00962
     """
 
-    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8,
+    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-6,
                  weight_decay=0, adam=False):
         if not 0.0 <= lr:
             raise ValueError("Invalid learning rate: {}".format(lr))
@@ -92,30 +91,34 @@ class Lamb(Optimizer):
 
                 state['step'] += 1
 
-                if group['weight_decay'] != 0:
-                    grad.add_(group['weight_decay'], p.data)
-
                 # Decay the first and second moment running average coefficient
+                # m_t
                 exp_avg.mul_(beta1).add_(1 - beta1, grad)
+                # v_t
                 exp_avg_sq.mul_(beta2).addcmul_(1 - beta2, grad, grad)
-                denom = exp_avg_sq.sqrt().add_(group['eps'])
+                denom = exp_avg_sq.sqrt().add(group['eps'])
 
-                bias_correction1 = 1 - beta1 ** state['step']
-                bias_correction2 = 1 - beta2 ** state['step']
+                # Paper v3 does not use debiasing.
+                # bias_correction1 = 1 - beta1 ** state['step']
+                # bias_correction2 = 1 - beta2 ** state['step']
                 # Apply bias to lr to avoid broadcast.
-                step_size = group['lr'] * math.sqrt(bias_correction2) / bias_correction1
+                step_size = group['lr'] # * math.sqrt(bias_correction2) / bias_correction1
+
+                weight_norm = p.data.pow(2).sum().sqrt()
 
                 adam_step = exp_avg / denom
-                # L2 norm uses sum, but here since we're dividing, use mean to avoid overflow.
-                r1 = p.data.pow(2).mean().sqrt()
-                r2 = adam_step.pow(2).mean().sqrt()
-                r = 1 if r1 == 0 or r2 == 0 else  min(r1/r2, 10)
-                state['r1'] = r1
-                state['r2'] = r2
-                state['r'] = r
-                if self.adam:
-                    r = 1
+                if group['weight_decay'] != 0:
+                    adam_step.add_(group['weight_decay'], p.data)
 
-                p.data.add_(-step_size * r, adam_step)
+                adam_norm = adam_step.pow(2).sum().sqrt()
+                # TODO: clamp this?
+                trust_ratio = weight_norm / adam_norm
+                state['weight_norm'] = weight_norm
+                state['adam_norm'] = adam_norm
+                state['trust_ratio'] = trust_ratio
+                if self.adam:
+                    trust_ratio = 1
+
+                p.data.add_(-step_size * trust_ratio, adam_step)
 
         return loss
